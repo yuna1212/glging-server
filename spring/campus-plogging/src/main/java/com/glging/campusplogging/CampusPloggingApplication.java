8e762eb539a66b58e4a7f3834053f9786ddc43cf
yuna1212 <yuna1275@gmail.com>
2022-06-16 23:33:54 +0900

    스프링 프로젝트 생성

---
+++ spring/campus-plogging/src/main/java/com/glging/campusplogging/CampusPloggingApplication.java
@@ -0,0 +1,13 @@
+package com.glging.campusplogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusPloggingApplication.class, args);
+	}
+
+}
---
+++ spring/campus-plogging/src/test/java/com/glging/campusplogging/CampusPloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glging.campusplogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusPloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
