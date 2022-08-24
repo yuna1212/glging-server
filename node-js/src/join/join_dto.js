77afc050a166269f26525d91a198129595b71810
yuna1212 <yunachong26@gmail.com>
2022-08-24 16:25:46 +0900

    refactor: join dto
    - join의 dto builder 선언
    
    related to: #6

---
+++ node-js/src/join/join_dto.js
@@ -0,0 +1,45 @@
+/* TODO: Dto builder에 대한 abstract class 선언하기. */
+
+class DtoBuilder {
+  #dto;
+  constructor() {
+    this.#dto = {};
+  }
+
+  add_success(is_successed) {
+    this.#dto.success = is_successed;
+    return this;
+  }
+
+  add_description(description) {
+    this.#dto.description = description;
+    return this;
+  }
+
+  add_access_token(access_token) {
+    this.#dto.access_token = access_token;
+    return this;
+  }
+
+  add_refresh_token(refresh_token) {
+    this.#dto.refresh_token = refresh_token;
+    return this;
+  }
+
+  add_user(user) {
+    this.#dto.user = {
+      nickname: user.univ_cert_status === 0 ? user.nickname || user.student_id.toString() : null,
+      student_id: user.student_id,
+      user_id: user.user_id,
+      univ_cert_status: user.univ_cert_status,
+      profile_image: 'http://18.119.6.206:8001/PLOGGING-PROFILE-IMAGES/' + user.profile_image,
+    };
+    return this;
+  }
+
+  get_dto() {
+    return this.#dto;
+  }
+}
+
+exports.DtoBuilder = DtoBuilder;
